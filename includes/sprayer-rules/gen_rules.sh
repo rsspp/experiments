95dec26ae9c3691365fce4c02ee7529576fb8f48
Tom Barbette <barbette@kth.se>
2019-10-05 13:07:44 +0200

    Add missing rules for metron and sprayer

---
+++ includes/sprayer-rules/gen_rules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+for i in $(seq 1 128) ; do vim -c ":for i in range(0,255) | put ='flow create 0 ingress pattern eth src spec 00:00:00:00:00:'.printf('%02x',i).' src mask 00:00:00:00:00:ff / end actions queue index '.(i%${i}).' / end' | endfor | :wq" rules-256-${i}queue ; done
+
