86276a173babbe5c64002796e3d7a1ad5fad7482
Tom Barbette <barbette@kth.se>
2019-10-10 18:08:51 +0200

    Update migration with re-computed data

--- migration/plot.py
+++ migration/plot.py
@@ -1,14 +1,13 @@
+from common import *
+
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.lines import Line2D
 import matplotlib.ticker as ticker
 import pandas
 from os import sys
-from common import *
 series = [ "Shared", "Group-table" ]
 labels = [ "Shared", "Per-bucket" ]
 
-shift = np.array([-10, 2, 14]) - 2
 f = "migration-udp-cx5"
 
 
@@ -23,12 +22,18 @@ for s in series:
         c = pandas.read_csv(f + "/" + s + "NCPU.csv", header=None, engine="python",usecols=range(4),delim_whitespace=True)
         data_c_s.append(np.array(c))
 
+shift = [0,0,0,0,0,0]
+shift[0] = -min([min(d[:,0]) for d in data_a_s]) -  0.9
+shift[1] = -min([min(d[:,0]) for d in data_a_s]) + 0.1
+shift[2] = -min([min(d[:,0]) for d in data_b_s])
+shift[3] = -min([min(d[:,0]) for d in data_b_s])
+shift[4] = -min([min(d[:,0]) for d in data_c_s])
 
 fig, ax1 = plt.subplots()
 fig.set_size_inches(6,3)
 
 for i,data in enumerate(data_a_s):
-    ax1.plot(data[:,0] + shift[0], [np.mean(l) for l in data[:,1]], label=labels[i], color = graphcolor[i], linestyle=linestyles[i % len(series)], marker='None')
+    ax1.plot(data[:,0] + shift[i], [np.mean(l) for l in data[:,1]], label=labels[i], color = graphcolor[i], linestyle=linestyles[i % len(series)], marker='None')
 
 ax1.set_ylabel('Throughput (Gbps)', color=darker(colors[0]))
 ax1.set_xlabel('Time (s)')
@@ -38,39 +43,40 @@ ax1.tick_params(axis='y', labelcolor=darker(colors[0]))
 
 ax2 = ax1.twinx()
 ax2.set_ylabel("Latency (µs)", color=darker(colors[1]))
-ax2.set_ylim(0,8000)
+ax2.set_ylim(0,10000)
 ax2.tick_params(axis='y', labelcolor=darker(colors[1]))
 for i,data in enumerate(data_b_s):
-    ax2.plot(data[:,0] + shift[1], [np.mean(l) for l in data[:,1]], label=labels[i], color = graphcolor[i + 2], linestyle=linestyles[i % len(series)], marker='None') #markers[ int(i / 2)]
+    ax2.plot(data[:,0] + shift[i + 2] , [np.mean(l) for l in data[:,1]], label=labels[i], color = graphcolor[i + 2], linestyle=linestyles[i % len(series)], marker='None') #markers[ int(i / 2)]
 
 yTickPos,_ = plt.yticks()
 yTickPos = yTickPos[:-1]
 data = data_c_s[1]
 h=max([max(data_b[:,1]) for data_b in data_b_s])
-h=8000
+h=10000
 import pandas as pd
 
 df = pd.DataFrame(data)
 df = df.groupby(df.columns[1], as_index=False).first().reset_index()
-df = df.sort_values(by=[df.columns[2]]).as_matrix()
+df = df.sort_values(by=[df.columns[2]]).values
 
 #Background
 t = np.asarray(df[:,2])
 w = np.asarray(np.ediff1d([0] + t))
 t=t[:-1]
-b = ax2.bar((t + w / 2) - shift[2], height=h, width=w, color=[lighter(colors[2],.12,1),lighter(colors[2],.05,1)], zorder=-99999)
+b = ax2.bar((t + w / 2) + shift[4], height=h, width=w, color=[lighter(colors[2],.12,1),lighter(colors[2],.05,1)], zorder=-99999)
 
 ax1.set_zorder(ax2.get_zorder()+1)
 ax1.patch.set_visible(False)
 for i, rect in enumerate(b):
     ax2.text(rect.get_x() + rect.get_width()/2, 0, '%d' % int(df[i,1]), ha='center', va='bottom', color=darker(colors[2]))
-xy=(b[2].get_x() + b[2].get_width()/2, 8000/12)
-ax2.annotate('# of cores',
-            xy=xy, xycoords='data',
-            xytext=(0.22,0.3), textcoords='axes fraction',
-	    color = darker(colors[2]),
-            arrowprops=dict(facecolor=darker(colors[2]), shrink=0.1,width=2,headwidth=4,headlength=4),
-            horizontalalignment='center', verticalalignment='top')
+
+#xy=(b[2].get_x() + b[2].get_width()/2, 8000/12)
+#ax2.annotate('# of cores',
+#            xy=xy, xycoords='data',
+#            xytext=(0.22,0.3), textcoords='axes fraction',
+#	    color = darker(colors[2]),
+#            arrowprops=dict(facecolor=darker(colors[2]), shrink=0.1,width=2,headwidth=4,headlength=4),
+#            horizontalalignment='center', verticalalignment='top')
 
 
 leg = ax1.legend(loc="lower left", title="Throughput", bbox_to_anchor = (0, 1))
