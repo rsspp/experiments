c21ce181699a1c8d8d28b18ff2cfc298072f032b
Tom Barbette <barbette@kth.se>
2019-10-19 12:31:56 +0200

    No need to create that many rules

--- includes/sprayer-rules/gen_rules.sh
+++ includes/sprayer-rules/gen_rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in $(seq 1 32) ; do
-    rm rules-256-${i}queue
+for i in $(seq 1 32) ; do 
+    rm rules-256-${i}queue &> /dev/null
     vim -c ":for i in range(0,255) | put ='flow create 0 ingress pattern eth src spec 00:00:00:00:00:'.printf('%02x',i).' src mask 00:00:00:00:00:ff / end actions queue index '.(i%${i}).' / end' | endfor | :wq" rules-256-${i}queue
 done
 
